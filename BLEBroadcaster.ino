84070aeb509fdc39d5bd35986beb38c02dff2d9f
Rishabh Tiwari <74355332+halfgamer@users.noreply.github.com>
2026-07-22 20:46:28 -0400

    Update BLEBroadcaster.ino

--- BLEBroadcaster.ino
+++ BLEBroadcaster.ino
@@ -1,16 +1,13 @@
 /*
  * SoleSense - BLE pressure broadcaster (Arduino Nano 33 BLE Sense Rev2)
  *
- * Reads 6 FSR channels and broadcasts them over BLE as a single
- * 12-byte packet (6 sensors x 2 bytes each, uint16, little-endian).
- *
  * ANALOG PIN ASSIGNMENT (sensor -> pin):
- *   A0 = Middle Joint
- *   A1 = Big Toe
- *   A2 = Right Joint
- *   A3 = Right Heel
- *   A4 = Left Heel
- *   A5 = Left Joint
+ *   A0 = Left Joint
+ *   A1 = Right Joint
+ *   A2 = Middle Joint
+ *   A3 = Left Heel
+ *   A4 = Right Heel
+ *   A5 = Big Toe
  */
 
 #include <ArduinoBLE.h>
@@ -48,20 +45,20 @@ void loop() {
     Serial.println(central.address());
 
     while (central.connected()) {
-      uint16_t middleJoint = analogRead(A0);
-      uint16_t bigToe      = analogRead(A1);
-      uint16_t rightJoint  = analogRead(A2);
-      uint16_t rightHeel   = analogRead(A3);
-      uint16_t leftHeel    = analogRead(A4);
-      uint16_t leftJoint   = analogRead(A5);
+      uint16_t leftJoint   = analogRead(A0);
+      uint16_t rightJoint  = analogRead(A1);
+      uint16_t middleJoint = analogRead(A2);
+      uint16_t leftHeel    = analogRead(A3);
+      uint16_t rightHeel   = analogRead(A4);
+      uint16_t bigToe      = analogRead(A5);
 
       uint8_t packet[12];
-      packet[0]  = middleJoint & 0xFF; packet[1]  = middleJoint >> 8;
-      packet[2]  = bigToe      & 0xFF; packet[3]  = bigToe      >> 8;
-      packet[4]  = rightJoint  & 0xFF; packet[5]  = rightJoint  >> 8;
-      packet[6]  = rightHeel   & 0xFF; packet[7]  = rightHeel   >> 8;
-      packet[8]  = leftHeel    & 0xFF; packet[9]  = leftHeel    >> 8;
-      packet[10] = leftJoint   & 0xFF; packet[11] = leftJoint   >> 8;
+      packet[0]  = leftJoint   & 0xFF; packet[1]  = leftJoint   >> 8;
+      packet[2]  = rightJoint  & 0xFF; packet[3]  = rightJoint  >> 8;
+      packet[4]  = middleJoint & 0xFF; packet[5]  = middleJoint >> 8;
+      packet[6]  = leftHeel    & 0xFF; packet[7]  = leftHeel    >> 8;
+      packet[8]  = rightHeel   & 0xFF; packet[9]  = rightHeel   >> 8;
+      packet[10] = bigToe      & 0xFF; packet[11] = bigToe      >> 8;
 
       pressureChar.writeValue(packet, 12);
       delay(50); // ~20Hz
